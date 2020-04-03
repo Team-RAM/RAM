374bf88b0bbe8010f3fd5e2a96b87457bd4bc32e
Yoosk <yooskacz@gmail.com>
2020-04-03 14:35:36 +0100

    move mekanism scripts to kubejs

---
+++ overrides/kubejs/data/modpack/kubejs/Mekanism/config.js
@@ -0,0 +1,9 @@
+ function  recipesFunction(e) {
+//Remove Anchor Upgrade (Now distributed by quest)
+  e.remove({output: 'mekanism:upgrade_anchor'});
+
+//Remove Wind Generator (Now distributed by quest)
+  e.remove({output: 'mekanismgenerators:wind_generator'});
+}
+
+events.listen(['recipes'], recipesFunction );
--- overrides/scripts/Mek/Mek_Config.zs
+++ overrides/scripts/Mek/Mek_Config.zs
@@ -5,9 +5,10 @@ Remove recipe for anchor upgrade - Yamza
 */
 
 
-
+/*
 //Remove Anchor Upgrade (Now distributed by quest)
 craftingTable.removeRecipe(<item:mekanism:upgrade_anchor>);
 
 //Remove Wind Generator (Now distributed by quest)
 craftingTable.removeRecipe(<item:mekanismgenerators:wind_generator>);
+*/
