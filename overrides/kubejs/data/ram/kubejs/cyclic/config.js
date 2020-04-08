59a0cf4a487362a461118d9b4e92cbf3a8ff3460
Yoosk <yooskacz@gmail.com>
2020-04-08 18:49:43 +0100

    ... and reusable ender pearl

--- overrides/kubejs/data/ram/kubejs/cyclic/config.js
+++ overrides/kubejs/data/ram/kubejs/cyclic/config.js
@@ -7,8 +7,5 @@ function recipesFunction(e) {
     e.remove({
         output: 'cyclic:tile_transporter_empty'
     });
-    e.remove({
-        output: 'cyclic:ender_eye_reuse'
-    });
 }
 events.listen(['recipes'], recipesFunction);
